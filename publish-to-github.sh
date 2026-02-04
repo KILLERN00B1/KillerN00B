ddb388790bd006e4654c74b0bf7b6f8ef0d21832
KillerN00B <mohamed8how@outlook.com>
2026-02-04 13:21:53 -0500

    Add files via upload

---
+++ publish-to-github.sh
@@ -0,0 +1,7 @@
+# from your site folder
+git init
+git add .
+git commit -m "Initial site"
+git branch -M main
+git remote add origin git@github.com:KILLERN00B1/bio-site.git
+git push -u origin main
